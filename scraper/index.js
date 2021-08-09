0a7fedd70dcbde8d93c8e9bd66fb9c2ea156e830
Carlos Guerrero López <c.guerrerolopez@gmail.com>
2021-08-09 13:15:36 +0200

    delete  log

--- scraper/index.js
+++ scraper/index.js
@@ -3,7 +3,6 @@ const scraperController = require('./pageController');
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob('0 1 * * *', function() {
-      console.log("2");
     //Start the browser and create a browser instance
     let browserInstance = browserObject.startBrowser();
     // Pass the browser instance to the scraper controller
